88b6d42f5baedec788c1f469847dbb866274286a
chris e <chriseickemeyer0000@gmail.com>
2019-10-14 19:37:38 -0500

    deleted extra brace

--- src/Components/VoteView.js
+++ src/Components/VoteView.js
@@ -39,43 +39,7 @@ export const VoteView = ({
           </div>
         ))}
       </div>
-      <PageForwardButton onClick={onPageForwardClick} />}
+      <PageForwardButton onClick={onPageForwardClick} />
     </>
   );
 };
-
-/*
-
-
-const rankerMain = () => (
-    <>
-      <NameScoreToggleButton state={charState} onToggle={voteNameToggle} />
-      {loading && <Spinner />}
-      <div className="flexMain">
-        {heroesArr.map((item, i) => (
-          <div className="flexMainChild" key={item.key}>
-            <VotingButtons
-              loggedIn={UID}
-              onUpvote={() => onUpvote(i)}
-              onDownvote={() => onDownvote(i)}
-              upvoteClass={
-                UID && item.upvoters.includes(UID) ? "icon iconActive" : "icon"
-              }
-              downvoteClass={
-                UID && item.downvoters.includes(UID)
-                  ? "icon iconActive"
-                  : "icon"
-              }
-              votes={item.votes}
-            />
-            <b className="inline titleSize">
-              <p>{item.name}</p>
-            </b>
-          </div>
-        ))}
-      </div>
-      <PageForwardButton onClick={onPageForward} />}
-    </>
-  );
-
-*/
